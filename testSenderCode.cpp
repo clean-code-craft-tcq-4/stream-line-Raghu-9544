019536d5bc0e56647a7c498d1fd31894845a2604
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-10-16 20:34:01 +0530

    Update testSenderCode.cpp

--- testSenderCode.cpp
+++ testSenderCode.cpp
@@ -1,4 +1,9 @@
 
 #include "test/catch.hpp"
+#include <time.h>
 #include <stdlib.h>
 #include "senderCode.h"
+
+
+sensorThresholdValues tempthresoldValues = {.maximumValue = 50.0, .minimumValue=10.0};
+sensorThresholdValues socthresoldValues = {.maximumValue = 85.0, .minimumValue=15.0};
